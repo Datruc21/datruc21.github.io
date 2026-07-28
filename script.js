4d6e4a026274fdde5445ccce81f787d0afe33c8c
jalil <jalil.bellahcen@gmail.com>
2026-07-28 10:19:21 +0200

    Finishing scrolling and adding content

--- script.js
+++ script.js
@@ -3,17 +3,51 @@ const main = document.querySelector("main")
 const positions = {
     home:     { x: -100, y: 0 }, 
     resume:   { x: 0,    y: 0 },   
-    about:    { x: -200, y: 0 },   // -200vw pour amener la colonne 3 au centre
-    projects: { x: -100, y: -100 } // -100vw pour rester en colonne 2, -100vh pour monter la ligne 2
+    about:    { x: -200, y: 0 },   
+    projects: { x: -100, y: -100 } 
 };
+
+let current_location = 'home'
+
 
 const goTo = (page) => {
     if (positions[page]) {
         const {x , y} = positions[page]
             main.style.transform = `translate(${x}vw, ${y}vh)`;
+            current_location = page
     } 
 }
 
 
-window.addEventListener('DOMContentLoaded', () => {
-    goTo('home');});
+window.addEventListener("keydown", (event) => {
+    const key = event.key;
+    switch (key) {
+        case "ArrowUp": 
+            if (current_location == 'projects') {
+                goTo('home');
+            }
+            break;
+        case "ArrowDown":
+            if (current_location == 'home') {
+                goTo('projects');
+            }
+            break;
+        case "ArrowLeft":
+            if (current_location == 'home') {
+                goTo('resume');
+            }
+            else if (current_location == 'about') {
+                goTo('home')
+            }
+            break;
+        case "ArrowRight":
+            if (current_location == 'home') {
+                goTo('about');
+            }
+            else if (current_location == 'resume') {
+                goTo('home')
+            }
+            break;
+
+    }
+})
